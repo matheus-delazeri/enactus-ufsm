d90a9d64300ea6588fb84e8d7b66bc2ac209263e
Matheus Delazeri <matheusdelazeris@gmail.com>
2023-07-24 22:56:49 -0300

    build: Added fontawesome package for icons

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
     resolve: {
         alias: {
             '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+            '~@fortawesome': path.resolve(__dirname, 'node_modules/@fortawesome'),
             '$': 'jQuery'
         }
     }
